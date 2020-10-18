1c3afeebb5367e112b85e2d9d70fad44810f4534
Chufan Chen <allenplato28@gmail.com>
2020-10-18 22:02:41 +0800

    finish my-cat

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BUFFER_SIZE 10000
+int main(int argc, char* argv[]) {
+  if (argc == 1) {
+    exit(0);
+  }
+  int i;
+  for (i = 1; i < argc; i++) {
+    FILE* fp = fopen(argv[i], "r");
+    if (fp == NULL) {
+      printf("wcat: cannot open file\n");
+      exit(1);
+    }
+    char buffer[BUFFER_SIZE];
+    while (fgets(buffer, BUFFER_SIZE, fp) != NULL) {
+      printf("%s", buffer);
+    }
+  }
+  return 0;
+}
