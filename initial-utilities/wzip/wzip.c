1760ea12c918bc9cd3c539eca4416f34a67d86e6
Chufan Chen <allenplato28@gmail.com>
2020-10-19 17:00:59 +0800

    finish wzip

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -5,13 +5,34 @@ int main(int argc, char* argv[]) {
     printf("wzip: file1 [file2 ...]\n");
     exit(1);
   }
-  ssize_t nread;
+  int count = 1;
+  int curr = '\0';
+  int last = '\0';
   for (int i = 1; i < argc; i++) {
     FILE* fp = NULL;
     if ((fp = fopen(argv[i], "r")) == NULL) {
       printf("wzip: cannot open file\n");
       exit(1);
     }
+    if (last == '\0') {
+      last = getc(fp);
+    }
+    while ((curr = getc(fp)) != EOF) {
+      if (curr == last) {
+        count++;
+      } else {
+        fwrite(&count, 4, 1, stdout);
+        fwrite(&last, 1, 1, stdout);
+        count = 1;
+        last = curr;
+      }
+    }
+    if (argc - 1== i) {
+      fwrite(&count, 4, 1, stdout);
+      fwrite(&last, 1, 1, stdout);
+      count = 1;
+    }
+
     fclose(fp);
   }
   return 0;
