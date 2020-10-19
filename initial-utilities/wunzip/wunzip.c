2bf42f338c3b15c11f9e030a093a44af9a6a54f9
Chufan Chen <allenplato28@gmail.com>
2020-10-19 17:21:25 +0800

    finish wunzip

--- initial-utilities/wunzip/wunzip.c
+++ initial-utilities/wunzip/wunzip.c
@@ -5,5 +5,24 @@ int main(int argc, char* argv[]) {
     printf("wunzip: file1 [file2 ...]\n");
     exit(1);
   }
+  FILE *fp = NULL;
+  for (int i = 1; i < argc; i++) {
+    if ((fp = fopen(argv[i], "r")) == NULL) {
+      printf("wunzip: cannot open file\n");
+      exit(1);
+    }
+    int count;
+    char ch;
+    while (fread(&count, 4, 1, fp) == 1) {
+      if (fread(&ch, 1, 1, fp) == 1) {
+        for (int j = 0; j < count; j++) {
+          printf("%c", ch);
+        }
+      } else {
+        printf("wunzip: file format mismatch\n");
+        exit(1);
+      }
+    }
+  }
   return 0;
 }
